b08bdacf9492ce7b3552643b8805dfe247e204e0
Gursimran Singh <gurisignh262001@gmail.com>
2022-02-10 20:34:16 +0530

    putting each row in separate folders with punjabi folder names improved but have bugs

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 import numpy as np
 
 import os
-
+from  googletrans import Translator
 import pytesseract as tess
 tess.pytesseract.tesseract_cmd= r'D:\tesseract-ocr\tesseract.exe'
 from PIL import Image
@@ -25,26 +25,32 @@ img = cv2.imread('testimage.jpeg')
 img= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
-path = "./"
+path = "./data"
 os.chdir(path)
 
 outer=[]
 i=1
 y=150
+k=1
 while (i<=(img.shape)[0]):
     image=img[i:y,0:1106];
     cv2.imwrite("data.jpeg",image)
     text = tess.pytesseract.image_to_string(Image.open("data.jpeg"), lang='pan')
-    arr=text.split(" ")
-    folder = "city"+arr[1][0]
+    arr=text.split(" ") and text.split("\n")
+    print(arr[0])
+    translator = Translator()
+
+    punjabitoeng= translator.translate(str(arr[0]),dest="en")
+    folder = str(k)+"c"+str(punjabitoeng.text)
     os.makedirs(folder)
-    cv2.imwrite(str(folder)+"/data"+str(arr[1][0])+ ".jpeg", image)
+    cv2.imwrite(str(folder)+"/d"+str(punjabitoeng.text)+str(k)+ ".jpeg", image)
     if(text):
         outer.append(text.split("\n"))
     i=y;
     y=y+150;
     cv2.imshow("cropped",image)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
+    k+=1
 
 for i in outer:
     print(i,"\n")
